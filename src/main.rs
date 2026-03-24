6393975541a649ffa66207cec457082c24b64567
cora <17833654450@163.com>
2026-03-24 13:56:19 +0800

    move get_arg() to utils mod

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,8 @@ use std::{path::PathBuf};
 mod task;
 mod utils;
 
-fn get_arg() -> Option<String> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.is_empty() || args.len() < 2 {
-        return None;
-    }
-
-    let arg = args[1].clone();
-    Some(arg.clone())}
-
 fn main() {
-    let Some(task_path) = get_arg() else {
+    let Some(task_path) = utils::get_arg() else {
         utils::error_and_exit("Task file is invivid".to_string())
     };
 
--- src/utils.rs
+++ src/utils.rs
@@ -2,3 +2,13 @@ pub fn error_and_exit(msg: String) -> ! {
     std::eprintln!("{}", msg);
     std::process::exit(-1);
 }
+
+pub fn get_arg() -> Option<String> {
+    let args: Vec<String> = std::env::args().collect();
+    if args.is_empty() || args.len() < 2 {
+        return None;
+    }
+
+    let arg = args[1].clone();
+    Some(arg.clone())
+}
